764ef4790a31e991233b4789707fe423ef9876aa
yoviarpauzi <yoviarpauzi99@gmail.com>
2022-10-10 16:47:13 +0700

    initial commit

---
+++ Sesi 3/src/App.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class App {
+
+  public static char grade(int a) {
+    if (a >= 0 && a < 50) {
+      System.out.println("Grade : E");
+      return 'E';
+    } else if (a >= 50 && a < 60) {
+      System.out.println("Grade : D");
+      return 'D';
+    } else if (a >= 60 && a < 70) {
+      System.out.println("Grade : C");
+      return 'C';
+    } else if (a >= 70 && a < 80) {
+      System.out.println("Grade : B");
+      return 'B';
+    } else {
+      System.out.println("Grade: A");
+      return 'A';
+    }
+  }
+  public static boolean status(char a){
+    if(a == 'D' || a == 'E'){
+        return false;
+    }else{
+        return true;
+    }
+  }
+  public static void Equal(){
+    for(int i = 0; i < 40; i++){
+        System.out.print("=");
+    }
+    System.out.print("\n");
+  }
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    Mahasiswa mhs = new Mahasiswa();
+    int n = 4;
+    String[] nama = new String[n];
+    boolean[] status = new boolean[n];
+    char[] grade = new char[n];
+    String[] nim = new String[n];
+    int[] nilai = new int[n];
+    for (int i = 0; i < n; i++) {
+      System.out.print("NIM   : ");
+      nim[i] = input.nextLine();
+      System.out.print("Nama  : ");
+      nama[i] = input.nextLine();
+      System.out.print("Nilai : ");
+      nilai[i] = input.nextInt();
+      while (nilai[i] < 0 || nilai[i] > 100) {
+        System.out.println("Inputan anda salah!");
+        System.out.print("Nilai : ");
+        nilai[i] = input.nextInt();
+      }
+      grade[i] = grade(nilai[i]);
+      status[i] = status(grade[i]);
+      input.nextLine();
+      Equal();
+    }
+    mhs.setDataMhs(nama, nim, nilai, grade, status);
+    System.out.println("Jumlah mahasiswa : " + n);
+    mhs.jmlLulus(status, true);
+    mhs.jmlLulus(status, false);
+    mhs.jmlnilai(grade, nama, 'A');
+    mhs.jmlnilai(grade, nama, 'B');
+    mhs.jmlnilai(grade, nama, 'C');
+    mhs.jmlnilai(grade, nama, 'D');
+    mhs.jmlnilai(grade, nama, 'E');
+    mhs.rataMhs(nilai);
+    input.close();
+  }
+}
---
+++ Sesi 3/src/Mahasiswa.java
@@ -0,0 +1,106 @@
+public class Mahasiswa {
+    private String[] nama, nim;
+    private char[] grade;
+    private boolean[] status;
+    private int[] nilai;
+    public void setDataMhs(String[] nama, String[] nim, int[] nilai, char[] grade, boolean[] status){
+        this.nama = nama;
+        this.nim = nim;
+        this.nilai = nilai;
+        this.grade = grade;
+        this.status = status;
+        nama = nim = null;
+        grade = null;
+        nilai = null;
+        status = null;
+    }
+    public String[] getNama(){
+        return nama;
+    }
+    public String[] getNim(){
+        return nim;
+    }
+    public int[] getNilai(){
+        return nilai;
+    }
+    public char[] getGrade(){
+        return grade;
+    }
+    public boolean[] getStatus(){
+        return status;
+    }
+    public int jmlMhs(String[] nama){
+        return nama.length;
+    }
+    public void jmlLulus(boolean[] status, boolean a){
+        int b = 0;
+        String c = "";
+        if(a){
+            c = "Lulus";
+        }else{
+            c = "Tidak Lulus";
+        }
+        for(int i = 0; i < status.length; i++){
+            if(status[i] == a){
+                b++;
+            }
+        }
+        if(b != 0){
+            System.out.printf("Jumlah mahasiswa yang %s : %d", c, b);
+            mhsLulus(getStatus(), getNama(), a);
+        }
+    }
+    public void mhsLulus(boolean[] status, String[] nama, boolean a){
+        System.out.print(" yaitu ");
+        int b = 0;
+        for(int i = 0; i < status.length; i++){
+            if(status[i] == a){
+                b++;
+                if(b >= 2){
+                    System.out.print(", ");
+                }
+                System.out.print(nama[i]);
+            }
+        }
+        System.out.print("\n");
+    }
+    public void jmlnilai(char[] grade, String[] nama, char a){
+        int b = 0;
+        for(int i = 0; i < grade.length; i++){
+            if(grade[i] == a){
+                b++;
+            }
+        }
+        if(b != 0){
+            System.out.printf("Jumlah mahasiswa dengan nilai %c : %d", a, b);
+            mhsNilai(getGrade(), getNama(), a);
+        }
+    }
+    public void mhsNilai(char[] grade, String[] nama, char a){
+        System.out.print(" yaitu ");
+        int b = 0;
+        for(int i = 0; i < grade.length; i++){
+            if(grade[i] == a){
+                b++;
+                if(b >= 2){
+                    System.out.print(", ");
+                }
+                System.out.print(nama[i]);
+            }
+        }
+        System.out.print("\n");
+    }
+    public void rataMhs(int[] nilai){
+        System.out.print("Rata-rata nilai mahasiswa adalah : ");
+        int b = nilai.length;
+        int a = 0;
+        for(int i = 0; i < nilai.length; i++){
+            System.out.print(nilai[i]);
+            a += nilai[i];
+            if(i != nilai.length - 1){
+                System.out.print("+");
+            }
+        }
+        System.out.printf(" / %d = %.2f", b, (float) a / b);
+    }
+}
